4a69e75882840506126a5a9a55a4445bdcb3377d
Jamey Wicklund <theBluWiz@icloud.com>
2022-11-30 20:42:16 -0800

    Adds prompting logic

--- __tests__/class.test.js
+++ __tests__/class.test.js
@@ -32,14 +32,13 @@ describe("Manager", () => {
     })
   })
 
-  describe("createCard", () => {
-    it("should generate an HTML string that would build a card for the employee and append that string to a cards variable", () => {
-      let cards = ""
+  describe("createHTMLString", () => {
+    it("should generate an HTML string that would build a card for the manager", () => {
       let goodHTML = '<div class="card"><div class="title"><h2>Monique</h2><h3>Manager</h3></div><div class="info"><div><p>ID: 1</p></div><div><p>Email: <a href="mailto:monique@corp.com">monique@corp.com</a></p></div><div><p>Office Number: R077</p></div></div></div>'
 
       const manager = new Manager(1, "Monique", "monique@corp.com", "R077");
-      
-      expect(manager.createHTMLString(cards)).toEqual(goodHTML)
+
+      expect(manager.createHTMLString()).toEqual(goodHTML)
     })
   })
 })
@@ -60,6 +59,16 @@ describe("Engineer", () => {
       expect(engineer.email).toEqual("engineer@corp.com")
     })
   })
+
+  describe("createHTMLString", () => {
+    it("should generate an HTML string that would build a card for the engineer", () => {
+      let goodHTML = '<div class="card"><div class="title"><h2>Trenton</h2><h3>Engineer</h3></div><div class="info"><div><p>ID: 1</p></div><div><p>Email: <a href="mailto:trenton@corp.com">trenton@corp.com</a></p></div><div><p>GitHub: DevOpsPro</p></div></div></div>'
+
+      const engineer = new Engineer(1, "Trenton", "trenton@corp.com", "DevOpsPro");
+
+      expect(engineer.createHTMLString()).toEqual(goodHTML)
+    })
+  })
 })
 
 describe("Intern", () => {
@@ -76,6 +85,16 @@ describe("Intern", () => {
       expect(intern.id).toEqual(1)
       expect(intern.name).toEqual("Eric")
       expect(intern.email).toEqual("intern@corp.com")
+    })
+  })
+
+  describe("createHTMLString", () => {
+    it("should generate an HTML string that would build a card for the intern", () => {
+      let goodHTML = '<div class="card"><div class="title"><h2>Gwen</h2><h3>Intern</h3></div><div class="info"><div><p>ID: 1</p></div><div><p>Email: <a href="mailto:gwen@corp.com">gwen@corp.com</a></p></div><div><p>Partner School: UW</p></div></div></div>'
+
+      const intern = new Intern(1, "Gwen", "gwen@corp.com", "UW");
+
+      expect(intern.createHTMLString()).toEqual(goodHTML)
     })
   })
 })
--- index.js
+++ index.js
@@ -1,11 +1,108 @@
 const fs = require("fs");
 const inquirer = require("inquirer")
-const Employee = require("../lib/EmployeeClass.js")
-const Manager = require("../lib/ManagerClass.js")
-const Engineer = require("../lib/EngineerClass.js")
-const Intern = require("../lib/InternClass.js")
+const Employee = require("./lib/EmployeeClass.js")
+const Manager = require("./lib/ManagerClass.js")
+const Engineer = require("./lib/EngineerClass.js")
+const Intern = require("./lib/InternClass.js")
+const prompt = require("./src/prompt")
 
-const manager = new Manager(1, "Monique", "manager@corp.com", "R077");
+initialQuestions = [
+  {
+    type: 'input',
+    message: "Team Manager Name:  ",
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: "Employee ID:  ",
+    name: 'ID',
+  },
+  {
+    type: 'input',
+    message: "Email Address:  ",
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: "Office Number:  ",
+    name: 'office',
+  },
+  {
+    type: 'input',
+    message: "Do you have additional team members to add to this profile?",
+    choices: [
+      "Engineer",
+      "Intern",
+      "All team members have been added"
+    ],
+    name: 'office',
+  },
+]
 
-manager.logStuff
-console.log(manager.createHTMLString)
+engineerQuestions = [
+  {
+    type: 'input',
+    message: "Team Manager Name:  ",
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: "Employee ID:  ",
+    name: 'ID',
+  },
+  {
+    type: 'input',
+    message: "Email Address:  ",
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: "GitHub Username:  ",
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: "Do you have additional team members to add to this profile?",
+    choices: [
+      "Engineer",
+      "Intern",
+      "All team members have been added"
+    ],
+    name: 'team',
+  },
+]
+
+internQuestions = [
+  {
+    type: 'input',
+    message: "Team Manager Name:  ",
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: "Employee ID:  ",
+    name: 'ID',
+  },
+  {
+    type: 'input',
+    message: "Email Address:  ",
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: "University Partnership:  ",
+    name: 'school',
+  },
+  {
+    type: 'input',
+    message: "Do you have additional team members to add to this profile?",
+    choices: [
+      "Engineer",
+      "Intern",
+      "All team members have been added"
+    ],
+    name: 'team',
+  },
+]
+
+prompt(initialQuestions);
